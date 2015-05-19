d346ab41ee13c2a1f05c954fcf809d0c0759507c
Mike Rooke <mike.rooke@gmail.com>
2015-05-19 15:03:19 -0400

    Simplify and condense the naive python solution

--- python/main.py
+++ python/main.py
@@ -1,69 +1,62 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import json
-import re
-import pprint
-import collections
+import json, re, collections, codecs
 
 def Tree(): return collections.defaultdict(Tree)
 
-listings_data = []
-products_data = []
-
-prod_tree = Tree()
-
-with open('./data/listings.txt') as listings_file:
-    for line in listings_file:
-        listings_data.append(json.loads(line))
-
-with open('./data/products.txt') as products_file:
-    for line in products_file:
-        product = json.loads(line)
-        product['listings'] = list()
-        products_data.append(product)
-
-        product_tokens = product['product_name'].lower().replace('_', '-').split('-')
-        tree = prod_tree
-        while len(product_tokens) > 1:
-            tree = tree[product_tokens.pop(0)]
-
-        tree[product_tokens.pop(0)] = product
-
-def match_listing(tokens, tree, mlist):
-    if 'model' in tree:
+def search(tokens, tree, mlist):
+    if 'model' in tree: #i.e. a leaf in the tree
         mlist.append(tree)
     else:
         matches = set(tokens).intersection(tree)
         for match in matches:
-            match_listing(tokens, tree[match], mlist)
+            search(tokens, tree[match], mlist)
 
-match_count = 0
-close_match = 0
-    
-for listing in listings_data:
-    delimiters = " ", ",", "_", "-", "[", "]", "/", "+", ";", "®"
-    regexPattern = '|'.join(map(re.escape, delimiters))
-    listing_tokens = re.split(regexPattern, listing['title'].lower())
+def main():
 
-    mlist = list()
-    match_listing(listing_tokens, prod_tree, mlist)
-    level = 0
+    products = []
+    prod_tree = Tree()
+            
+    for line in codecs.open("./data/products.txt", encoding="UTF-8").readlines():
+        product = json.loads(line)
+        product['listings'] = list()
+        product_tokens = product['product_name'].lower().replace('_', '-').split('-')
 
-    if len(mlist) > 1: #i.e. multiple matches probably means dubious matchs
-        close_match += 1
-    elif len(mlist) > 0:
-        for product in mlist:
-            product['listings'].append(listing)
-            match_count += 1
+        tree = prod_tree        
+        while len(product_tokens) > 1:
+            tree = tree[product_tokens.pop(0)]
+        tree[product_tokens.pop(0)] = product
+        
+        products.append(product)
 
+    match_count = 0
+    total_listings = 0
 
-with open('results.txt', 'w') as fp:
-    for product in products_data:
-        result = {'product_name':product['product_name'], 'listings':product['listings']}
-        json.dump(result, fp)
-        fp.write('\n')
+    for line in codecs.open("./data/listings.txt", encoding="UTF-8").readlines():
+        listing = json.loads(line)
+        delimiters = " ", ",", "_", "-", "[", "]", "/", "+", ";", "®"
+        regex_pattern = '|'.join(map(re.escape, delimiters))
+        listing_tokens = re.split(regex_pattern, listing['title'].lower())
+        
+        mlist = list()
+        search(listing_tokens, prod_tree, mlist)
+        total_listings += 1
+        
+         # multiple (complete) matches probably means dubious matches, so allow only one
+        if len(mlist) == 1:
+            for product in mlist:
+                product['listings'].append(listing)
+                match_count += 1
 
-print('Match count: ' + str(match_count))
-print('Reject count: ' + str(len(listings_data) - match_count))
+    with open('results.txt', 'w') as fp:
+        for product in products:
+            result = {'product_name':product['product_name'], 'listings':product['listings']}
+            json.dump(result, fp)
+            fp.write('\n')
+
+    print('Match count: ' + str(match_count))
+    print('Reject count: ' + str(total_listings - match_count))
+
+main()
                     
